71d07df156add752433f6adad3cdb79c7a7278dd
Arnaud Clarat <arnaud.clarat@live.fr>
2024-10-05 10:17:09 +0200

    Premier commit

---
+++ marketplace.cs
@@ -0,0 +1,23 @@
+// Définition des variables
+string[] panier;
+string[] fruits = ["pomme", "poire", "pèche", "fraise", "banane", "mangue"]
+int code = 99;
+
+// Définition des méthodes
+int menu()
+{
+	Console.WriteLine(@"Gestion du panier de fruits
+Selectionnez votre choix :
+	1. Afficher tous les fruits disponibles
+	2. Afficher les fruits du panier
+	3. Ajouter un fruit au panier
+	4. Retirer un fruit du panier
+	0. Quitter le programme");
+	string input = Con
+}
+
+
+do
+{
+	menu()
+} while code !== 0
